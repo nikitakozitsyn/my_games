37f2999ac419540213c201b8dafa3126ef9077aa
Nikita <kozitsyn94vyatka@gmail.com>
2020-02-13 11:41:40 +0300

    release v3

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ class Matrix:
         update- flag for inserting [2] or [4] cell, lose- or win- flag, cells style,
         grid for storing cells values, cells for visual display grid.
         Checks result file if exists to show best score in current side length mode"""
-
         self.name = name.title() if name.isalpha() and len(name) < 7 else 'guest'
         self.side = int(side) if side.isdigit() and 9 > int(side) > 1 else 4
         self.score = 0
@@ -25,26 +24,24 @@ class Matrix:
         self.cells = np.array([[tk.Label(**self.style) for _ in range(self.side + 1)] for _ in range(self.side + 1)])
         for i in range(self.side + 1):
             for j in range(self.side + 1):
-                self.cells[i][j].config(bg='#333' if (i != self.side) ^ (j != self.side) else '#000', fg='White')
+                self.cells[i][j].config(bg='#333' if max(i, j) == 4 else '#000', fg='White')
                 self.cells[i][j].grid(row=i, column=j)
         if os.path.exists('results.csv'):
             with open('results.csv') as file:
                 results = [row for row in file.read().split('\n')[1:-1] if int(row.split(',')[1]) == self.side]
-                self.record = max(results, key=lambda row: (int(row.split(',')[0]), -results.index(row)))
-                self.cells[0][-1].config(text='Leader:', fg='#888')
-                self.cells[1][-1].config(text=f'{self.record.split(",")[2]}\n{self.record.split(",")[0]}', fg='#000')
+                self.record = max(results, key=lambda row: (int(row.split(',')[0]), -results.index(row))).split(',')
+                self.cells[0][-1].config(text=f'Leader:\n{self.record[2]}', fg='#888')
+                self.cells[1][-1].config(text=f'{self.record[0]}\n{self.record[3].split()[0]}', fg='#888')
         self.cells[-1][0].config(text=f'SCORE:\n{self.score}', fg='#888')
 
     def binds(self, event):
         """Binds events with methods"""
-
         eval(f'self.{event.keysym.lower()}()') if event.keysym in ('Left', 'Right', 'Up', 'Down') else None
         self.update_labels() if self.is_update else None
         self.save_and_exit() if event.keysym == 'Escape' else None
 
     def check(self):
         """Checks grid for lose- flag (if all cells are full)"""
-
         for row, column in zip(self.grid, self.grid.T):
             for i in range(self.side - 1):
                 if row[i] == row[i + 1] or column[i] == column[i + 1]:
@@ -53,7 +50,6 @@ class Matrix:
 
     def change_labels(self):
         """Transfers grid values to cells values"""
-
         for i in range(self.side):
             for j in range(self.side):
                 color = f'#0{int(math.log(self.grid[i][j], 2)) % 16:X}{int(math.log(self.grid[i][j], 2)) * 2 % 16:X}'
@@ -63,7 +59,6 @@ class Matrix:
 
     def move(self, array):
         """Moves all values in vector given the rules of game"""
-
         top, prev = 0, 1
         for i in range(len(array)):
             if array[i] != 1 and array[i] != prev:
@@ -76,13 +71,11 @@ class Matrix:
 
     def insert(self):
         """Inserts [2] or [4] value in grid in random choice"""
-
         cell = rd.choice([(i, j) for i in range(self.side) for j in range(self.side) if self.grid[i][j] == 1])
         self.grid[cell[0]][cell[1]] = rd.choice([2] * 9 + [4])
 
     def update_labels(self):
         """Updates grid by inserting value, checking for lose- or win- flag and transferring cells from grid"""
-
         self.insert()
         self.check() if sum(sum(self.grid > 1)) == self.side ** 2 else None
         self.is_win[0] = True if sum(sum(self.grid == 2 ** (self.side ** 2 - 2 * self.side + 3))) else self.is_win[0]
@@ -91,40 +84,34 @@ class Matrix:
 
     def save_and_exit(self):
         """Creates result file or update it if exists and exit"""
-
         with open('results.csv', 'a') as file:
             None if os.stat('results.csv').st_size else file.write('Score,Side,Name,Datetime\n')
-            file.write(f'{self.score},{self.side},{self.name},{dt.datetime.now().strftime("%d-%m-%Y %H:%M")}\n')
+            file.write(f'{self.score},{self.side},{self.name},{dt.datetime.now().strftime("%d-%m-%y %H:%M")}\n')
         sys.exit()
 
     def left(self):
         """Moves all values in grid given the rules of game left direction"""
-
         for row in self.grid:
             self.move(row)
 
     def right(self):
         """Moves all values in grid given the rules of game right direction"""
-
         for row in self.grid:
             self.move(row[::-1])
 
     def up(self):
         """Moves all values in grid given the rules of game up direction"""
-
         for column in self.grid.T:
             self.move(column)
 
     def down(self):
         """Moves all values in grid given the rules of game down direction"""
-
         for column in self.grid.T:
             self.move(column[::-1])
 
 
 def create(name, side, prev: tk.Label, root: tk.Tk):
     """Destroys previous window, calls Matrix object passing name and side length"""
-
     prev.destroy()
     matrix = Matrix(name.get() if type(name) != str else name, side.get() if type(side) != str else side)
     root.title(f'Hello, {matrix.name}!')
@@ -133,7 +120,6 @@ def create(name, side, prev: tk.Label, root: tk.Tk):
 
 def start(root: tk.Tk):
     """Calls start window(if no command line arguments), which requests name and side length"""
-
     root.title('start')
     name, side = tk.StringVar(), tk.StringVar()
     style = {'font': 'Arial', 'bg': '#333', 'fg': '#FFF'}
